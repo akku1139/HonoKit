7ac799c414712d7ca6a87a572e585fc4d09fd2fc
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-13 05:48:35 +0900

    memo

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,11 +5,24 @@ const root = "/src/routes";
 /// SvelteKit風なルーティングをHono風なルーティングに変換する
 // とりあえずフラットにルートを作るようにするけど
 // routeを入れ後にしたほうが良さそうならディレクトリごとで回す
+/* 最下位
+src/routes/[...catchall]/+page.svelte
+src/routes/[[a=x]]/+page.svelte
+src/routes/[b]/+page.svelte
+src/routes/foo-[c]/+page.svelte
+src/routes/foo-abc/+page.svelte
+一位
+*/
 export function createApp() {
   const routes: Record<string, object> = import.meta.glob(root+"/**/+*.(js|ts)", {
     eager: true,
   });
+
+  const app = new Hono();
+
   Object.keys(routes).forEach((file) =>{
-    file.split("/")./* 空白を除去 */filter((e) => Boolean(e))
+    file.replace(root, "").split("/").filter((e) => Boolean(e));
+    // ["api", "notes", "create", "+POST.js"]
+    [].at(-1);
   });
 }
