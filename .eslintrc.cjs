c85d06a236766232378ef31d2a1b6f45ecbd1671
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-13 03:09:06 +0900

    config

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+/** @type { import("eslint").Linter.Config } */
+module.exports = {
+  rules: {
+    semi: ["error", "always"],
+    "comma-dangle": ["error", "always-multiline"],
+    "no-else-return": ["error", {allowElseIf: false}],
+    "no-lonely-if": "error",
+    "no-tabs": "error",
+    "eol-last": ["error", "always"],
+  },
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte'],
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+};
