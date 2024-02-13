0f1d8b7dfc98f32b67dd87607d165c867dbe19b8
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-13 11:29:49 +0900

    a

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,6 +4,8 @@ const root = "/src/routes";
 
 // SvelteKit互換ルーティングはやめてHonoネイティブに
 // : はWindowsのPathに使えないらしいから[]で囲む
+// やっぱSvelteKit互換に仕上げようかな
+// routeの中でifを使うことも辞さない構え
 export function createApp(): Hono {
   const routes: Record<string, object> = import.meta.glob(root+"/**/+*.(js|ts)", {
     eager: true,
@@ -12,7 +14,7 @@ export function createApp(): Hono {
   const app = new Hono();
 
   Object.keys(routes).forEach((file) =>{
-    file.replace(root, "").split("/").filter((e) => Boolean(e));
+    const route = file.replace(root, "").split("/").filter((e) => Boolean(e));
     // ["api", "notes", "create", "+POST.js"]
     app[/(.+)\.(js|ts)/.exec([].at(-1).toLowerCase)]("いい感じに再構築したルート", route[file]["default"]);
   });
